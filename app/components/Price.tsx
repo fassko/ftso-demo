de7cabfade1be6f19bad47ed40a4491dbdd5e442
Kristaps Grinbergs <fassko@gmail.com>
2023-12-05 15:38:56 +0700

    Improve data loading

---
+++ app/components/Price.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+
+import { nameToAbi } from "@flarenetwork/flare-periphery-contract-artifacts";
+import { ethers, formatUnits, toNumber } from "ethers";
+
+interface PriceParams {
+  symbol: string;
+}
+
+interface PriceDetails {
+  price: string;
+  time: Date;
+}
+
+const FLARE_RCP = "https://flare-api.flare.network/ext/bc/C/rpc";
+const FLARE_CONTRACT_REGISTRY_ADDRESS =
+  "0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019";
+
+export default function Price({ symbol }: PriceParams) {
+  const [assetPrice, setAssetPrice] = useState<PriceDetails>();
+
+  function onRefresh() {
+    setAssetPrice(undefined);
+
+    getAssetPrice(symbol);
+  }
+
+  const getAssetPrice = useCallback(async (symbol: string) => {
+    console.log(`Getting ${symbol} price ...`);
+
+    const provider = new ethers.JsonRpcProvider(FLARE_RCP);
+
+    // get Flare contract registry
+    // // https://docs.flare.network/dev/getting-started/contract-addresses/
+    const flareContractRegistry = new ethers.Contract(
+      FLARE_CONTRACT_REGISTRY_ADDRESS,
+      nameToAbi("FlareContractRegistry", "flare").data,
+      provider
+    );
+
+    // get FTSO registry contract address
+    // https://docs.flare.network/apis/smart-contracts/FlareContractRegistry/
+    const ftsoRegistryAddress =
+      await flareContractRegistry.getContractAddressByName("FtsoRegistry");
+
+    // convert to FTSO registry contract
+    // https://docs.flare.network/apis/smart-contracts/FtsoRegistry/
+    const ftsoRegistry = new ethers.Contract(
+      ftsoRegistryAddress,
+      nameToAbi("FtsoRegistry", "flare").data,
+      provider
+    );
+
+    // Get current price from the FTSO
+    // https://docs.flare.network/apis/smart-contracts/FtsoRegistry/#fn_getcurrentpricewithdecimals_a69afdc6
+    const [price, timestamp, decimals] = await ftsoRegistry[
+      "getCurrentPriceWithDecimals(string)"
+    ](symbol);
+    const assetPrice = formatUnits(price, decimals);
+    const time = new Date(Number(timestamp) * 1000);
+
+    console.log(`Calculated at ${time}`);
+    // console.log(`${Number(price) / Math.pow(10, Number(decimals))} USD`);
+    console.log(`${formatUnits(price, decimals)} USD`);
+
+    let USDollar = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: Number(decimals),
+    });
+
+    setAssetPrice({ price: USDollar.format(Number(assetPrice)), time: time });
+  }, []);
+
+  useEffect(() => {
+    getAssetPrice(symbol);
+  }, [getAssetPrice, symbol]);
+
+  return (
+    <div
+      onClick={onRefresh}
+      className="py-2 px-4 mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl cursor-pointe bg-gray-50 hover:bg-gray-100 cursor-pointer gap-8 w-[300px]"
+    >
+      <h2 className="text-xl font-bold text-gray-900 block">{symbol}</h2>
+      {assetPrice ? (
+        <div>
+          <div className="flex">{assetPrice.price}</div>
+          <div>
+            {`${assetPrice.time.toLocaleTimeString()} ${assetPrice.time.toLocaleDateString()}`}
+          </div>
+        </div>
+      ) : (
+        <div>Loading</div>
+      )}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import Image from 'next/image'
-import AssetPrice from './components/AssetPrice'
+import Image from "next/image";
+import Price from "./components/Price";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-10 p-10">
-      <Image src="/flare-logo.svg" alt="Flare Logo" width={300} height={125} />
+    <main className="flex min-h-screen flex-col items-center gap-4 p-4">
+      <Image src="/flare-logo.svg" alt="Flare Logo" width={120} height={50} />
 
-      <div className='flex gap-4 flex-wrap flex-col'>
-        <AssetPrice symbol='FLR' />
-        <AssetPrice symbol='BTC' />
-        <AssetPrice symbol='ETH' />
-        <AssetPrice symbol='USDC' />
-        <AssetPrice symbol='USDT' />
-        <AssetPrice symbol='XRP' />
-        <AssetPrice symbol='MATIC' />
+      <div className="flex gap-4 flex-wrap flex-col">
+        <Price symbol="FLR" />
+        <Price symbol="BTC" />
+        <Price symbol="ETH" />
+        <Price symbol="XRP" />
+        <Price symbol="USDC" />
+        <Price symbol="USDT" />
+        <Price symbol="MATIC" />
       </div>
     </main>
-  )
+  );
 }
