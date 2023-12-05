2d32f7b28bfc6313b39d66876a6eda7a49213d11
Kristaps Grinbergs <fassko@gmail.com>
2023-12-05 16:22:27 +0700

    Improved layout and added links

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Price from "./components/Price";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-4 p-4">
-      <Image src="/flare-logo.svg" alt="Flare Logo" width={120} height={50} />
+      <Link href="https://flare.network/" target="_blank">
+        <Image src="/flare-logo.svg" alt="Flare Logo" width={120} height={50} />
+      </Link>
 
       <div className="flex gap-4 flex-wrap flex-col">
         <Price symbol="FLR" />
@@ -15,6 +18,18 @@ export default function Home() {
         <Price symbol="USDT" />
         <Price symbol="MATIC" />
       </div>
+
+      <p className="pt-6">
+        Prices are provided by{" "}
+        <Link
+          href="https://docs.flare.network/tech/ftso/"
+          target="_blank"
+          className="font-medium hover:underline"
+        >
+          Flare Time Series Oracle (FTSO)
+        </Link>
+        .
+      </p>
     </main>
   );
 }
